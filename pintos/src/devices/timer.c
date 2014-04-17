61020db217b0bbd18dc6c86f535b4a8c863d78c5
Matthew Uthainatapong <mutha001@hammer.cs.ucr.edu>
2014-04-16 19:05:22 -0700

    Updated thread_forsleep()

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -89,14 +89,13 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
-  int64_t start = timer_ticks ();
-  /*
+  /*int64_t start = timer_ticks ();
+  
   ASSERT (intr_get_level () == INTR_ON);
   while (timer_elapsed (start) < ticks) 
     thread_yield (); */
   ASSERT (intr_get_level () == INTR_ON);
   
-  printf("Calling thread_sleep(ticks)\n"); 
   thread_sleep(ticks);
 }
 
